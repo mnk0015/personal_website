8cfaf0dc0dadb025ec19e4380c7ad373af5cee3b
mnk0015 <mohammednkhan0015@gmail.com>
2024-07-28 13:31:03 -0400

    updated Header.jsx and added and updated Nav.jsx
    added a nav bar in the header and added some buttons that we would be able to use later to redirect to other links  and resources regarding my work.

---
+++ components/Nav.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+const links = [
+  {
+    name:"home",
+    path: "/",
+  },
+  {
+    name:"services",
+    path: "/services",
+  },
+  {
+    name:"resume",
+    path: "/resume",
+  },
+  {
+    name:"work",
+    path: "/work",
+  },
+  {
+    name:"contact",
+    path: "/contact",
+  },
+];
+
+const Nav = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+  return (
+    <nav className="flex gap-8">
+    {links.map((link, index) => {
+        return  (
+        <Link href={link.path} key={index} className={''}>
+            {link.name}
+        </Link>
+        );
+    })}
+    </nav>
+  );
+};
+
+export default Nav
