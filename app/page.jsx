e3666d436b9631721a6db9b9cd8be9286408f7c5
mnk0015 <mohammednkhan0015@gmail.com>
2024-07-28 14:13:35 -0400

    updated homepage and globals.css

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,21 @@
+
+
 export default function Home() {
   return (
-    <main>homepage</main>
+    <section>
+      <div className= "container mx-auto h-full">
+        <div className = "flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span> Software Developer </span>
+            <h1 className="h1">
+               Hello I'm <br /> <span className="text-accent">Mohammed Khan</span>
+            </h1>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
 }
